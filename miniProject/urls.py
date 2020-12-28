9ae7a6aa16dce2b045279b5ad74e33539cefb5b5
paramSonawane <paramsonawane@gmail.com>
2020-12-28 17:29:25 +0530

    added authentication app

--- miniProject/urls.py
+++ miniProject/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import url,include
+
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('home/', include('home.urls')),
+    url(r'^$',views.index,name='index'),
+    url(r'^special/',views.special,name='special'),
+    url(r'^accounts/',include('accounts.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
