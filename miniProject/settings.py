09aa18a18408f02fb286f1f9cebfc9c9f0e8c6a4
paramSonawane <paramsonawane@gmail.com>
2020-12-28 18:00:24 +0530

    changed db to sqlite3

--- miniProject/settings.py
+++ miniProject/settings.py
@@ -73,22 +73,22 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dmw',
-        'USER': 'sinisterstrike',
-        'PASSWORD': '8421',
-        'HOST': 'localhost'
-    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dmw',
+#         'USER': 'sinisterstrike',
+#         'PASSWORD': '8421',
+#         'HOST': 'localhost'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
